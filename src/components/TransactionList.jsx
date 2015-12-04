4fb1d3357213ae30a2152f90c799b70033df987e
John Mortlock <johnm@hlaustralia.com.au>
2015-12-04 11:55:38 +1030

    Get the transaction list up and running and bound to the sale_items state.

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-export default React.createClass({
-    render: function() {
-      return <div className="winner">
-        This is the transaction list.
-      </div>;
+export const TransactionList = React.createClass({
+  render: function() {
+    var createItem = function(item) {
+      return <li>{item.get("description")}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
   }
-
 });
+
+function mapStateToProps(state) {
+  return {
+    items: state.get('sale_items'),
+  };
+}
+
+export const TransactionListContainer = connect(mapStateToProps)(TransactionList);
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,24 @@ import Router, {Route} from 'react-router';
 import makeStore from './store'
 import {Provider} from 'react-redux';
 import App from './components/App';
-import TransactionList from './components/TransactionList';
+import {TransactionListContainer} from './components/TransactionList';
+import {createAction} from 'redux-actions';
+import {types} from './core/actions';
 
 const store = makeStore();
 
+const TestItem1 = {
+  plu:100,
+  description: 'Coopers Pale Ale',
+  prices: {A:1, B:2, C:3, D:6, E:9 }
+};
+
+const action = createAction(types.ADD_ITEM)( { item: TestItem1 });
+store.dispatch(action);
+store.dispatch(action);
+
 const routes = <Route component={App}>
-  <Route path="/" component={TransactionList} />
+  <Route path="/" component={TransactionListContainer} />
 </Route>
 
 ReactDOM.render(
